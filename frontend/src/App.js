64d3594aa073206aa05d468bdd868adc0785d055
Sagar Khadka <sagarkhadka2069@gmail.com>
2020-10-30 16:55:30 +0545

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Product from './components/Product';
+import data from './data';
+
+function App() {
+  return (
+	<div className="grid-container">
+	<header className="row">
+		<div>
+			<a className="brand" href="/">Galaxy Hardware</a>
+		</div>
+		<div>
+			<a href="/cart">Cart</a>
+			<a href="/signin">Sign In</a>
+		</div>
+	</header>
+	<main>
+		<div className="row center">
+		{
+			data.products.map((product) =>(
+				<Product key={product._id} product={product}></Product>
+				))}
+		</div>
+	</main>
+	<footer className="row center">All right reserved</footer>
+</div>
+  );
+}
+
+export default App;
